8bba43fb3f90856991d3fbfb1e0b5d5b90ed15c0
Mark-Anthony Hutton <mhutton86@gmail.com>
2024-08-31 20:04:13 -0500

    enhance super linter to use configuration for formatting and fix file ownership of formatted files

--- scripts/tools/super-linter.sh
+++ scripts/tools/super-linter.sh
@@ -10,31 +10,30 @@ PROJECT_ROOT=$(cd "${SCRIPT_DIR}/../.." && pwd)
 # Source the common functions and constants
 source "${SCRIPT_DIR}/../.common.sh"
 
+# Run super-linter as a Docker container. Success checked upon return.
 echo_yellow "Running super-linter using Docker..."
 
-# Run super-linter as a Docker container
+# Custom entrypoint command explanation:
+# The custom entrypoint command `/bin/sh -c "/action/lib/linter.sh; chown -R $(id -u):$(id -g) ${WORK_DIR}"` does the following:
+# 1. It first runs the default linter script (`/action/lib/linter.sh`), which lints and potentially fixes the codebase.
+# 2. Regardless of whether the linter script succeeds or fails, it then runs `chown -R $(id -u):$(id -g) ${WORK_DIR}`.
+#    This command changes the ownership of all files in the mounted workspace directory back to the user who invoked the Docker command.
+#    This step is necessary because Docker containers typically run as root by default, which can result in files being owned by root on the host system.
+#    Running `chown` ensures that file ownership is corrected, preventing permission issues when working with these files outside of Docker.
 
-# Check whether super-linter succeeded or failed.
+WORK_DIR="/tmp/lint" # Directory to mount as workspace inside super-linter container
 if docker run \
   --rm \
+  --env-file "${PROJECT_ROOT}/.super-linter-format-cfg.env" \
   -e RUN_LOCAL=true \
-  -e DEFAULT_WORKSPACE=/tmp/lint \
+  -e DEFAULT_WORKSPACE="${WORK_DIR}" \
   -e DEFAULT_BRANCH=main \
-  -e FIX_SHELL_SHFMT=true \
-  -e FIX_JSON_PRETTIER=true \
-  -e FIX_GOOGLE_JAVA_FORMAT=true \
-  -e FIX_MARKDOWN_PRETTIER=true \
-  -e FIX_POWERSHELL=true \
-  -e FIX_YAML_PRETTIER=true \
-  -e FIX_MARKDOWN_PRETTIER=true \
-  -v "${PROJECT_ROOT}:/tmp/lint" \
-  ghcr.io/super-linter/super-linter:v7; then
-
-  # TODO deduplicate ownership logic with a common function
-  # sudo chown -R "$(id -u):$(id -g)" "${PROJECT_ROOT}"
+  -v "${PROJECT_ROOT}:${WORK_DIR}" \
+  --entrypoint /bin/sh \
+  ghcr.io/super-linter/super-linter:v7 \
+  -c "/action/lib/linter.sh && chown -R $(id -u):$(id -g) ${WORK_DIR}"; then
   echo_green "Super-linter completed successfully."
 else
-  # sudo chown -R "$(id -u):$(id -g)" "${PROJECT_ROOT}"
   echo_red "Super-linter reported errors."
   exit 1
 fi
