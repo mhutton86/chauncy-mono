c94105598bf6dc13bc9558ee3ae4765234afb4fb
Mark-Anthony Hutton <mhutton86@gmail.com>
2024-09-08 19:39:31 -0500

    generalized super-linter lint cfg to file, disabled SL in GHA, share config for SL between GHA and CLI

--- scripts/tools/super-linter.sh
+++ scripts/tools/super-linter.sh
@@ -3,6 +3,12 @@
 #
 # Note: Despite Yaml prettier, bashfmt, and other mentioned prettifier tools. They are used only for linting purposes. As its name indicates, Super-Linter focuses on linting only. Formatting is intentionally supposed to be a separate sctivity.
 
+# Disable super-linter in GitHub Actions. It's setup to run in the GHA lint pipeline.
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo_yellow "Super-linter disabled in GitHub Actions..."
+  exit 0
+fi
+
 # Directories to simplify making script runnable from anywhere
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PROJECT_ROOT=$(cd "${SCRIPT_DIR}/../.." && pwd)
@@ -12,9 +18,6 @@ source "${SCRIPT_DIR}/../.common.sh"
 
 # Run super-linter as a Docker container. Success checked upon return.
 echo_yellow "Running super-linter using Docker..."
-
-# Set RUN_LOCAL based on whether the script is running within GitHub Actions
-RUN_LOCAL=$([ "$GITHUB_ACTIONS" = "true" ] && echo "false" || echo "true")
 
 # Custom entrypoint command explanation:
 # The custom entrypoint command `/bin/sh -c "/action/lib/linter.sh; chown -R $(id -u):$(id -g) ${WORK_DIR}"` does the following:
@@ -27,12 +30,10 @@ RUN_LOCAL=$([ "$GITHUB_ACTIONS" = "true" ] && echo "false" || echo "true")
 WORK_DIR="/tmp/lint" # Directory to mount as workspace inside super-linter container
 if docker run \
   --rm \
+  --env-file "${PROJECT_ROOT}/.super-linter-lint-cfg.env" \
   --env-file "${PROJECT_ROOT}/.super-linter-format-cfg.env" \
-  -e RUN_LOCAL="${RUN_LOCAL}" \
+  -e RUN_LOCAL=true \
   -e DEFAULT_WORKSPACE="${WORK_DIR}" \
-  -e DEFAULT_BRANCH=main \
-  -e LINTER_RULES_PATH=. \
-  -e EDITORCONFIG_FILE_NAME=.editorconfig \
   -v "${PROJECT_ROOT}:${WORK_DIR}" \
   --entrypoint /bin/sh \
   ghcr.io/super-linter/super-linter:v7 \
